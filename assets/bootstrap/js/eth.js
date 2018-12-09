8aeba69a740a62f65a6e6ee0a658c2c19653e023
Achudh Krishna <achudhk@gmail.com>
2018-12-09 16:31:20 +0530

    trying to edit sol

--- assets/bootstrap/js/eth.js
+++ assets/bootstrap/js/eth.js
@@ -4,4 +4,31 @@ if (typeof web3 !== 'undefined') {
     // Set the provider you want from Web3.providers
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
     web3.eth.defaultAccount = web3.eth.accounts[0];
+
+    web3.setProvider(new web3.providers.HttpProvider('http://' + BasicAuthUsername + ':' + BasicAuthPassword + '@localhost:8545'));
+    var NovelContract = web3.eth.contract([
+        {
+            "constant": false,
+            "inputs": [
+                {
+                    "name": "_writername",
+                    "type": "string"
+                },
+                {
+                    "name": "_membername",
+                    "type": "string"
+                },
+                {
+                    "name": "_associationname",
+                    "type": "string"
+                }
+            ],
+            "name": "ifScriptVaild",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]);
   }
+  
